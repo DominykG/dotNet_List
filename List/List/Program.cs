7b3cac67d949346abd04c1a1ce473a25e2b21b8f
Batzilla <gavrilovas.dominykas@gmail.com>
2019-04-01 23:50:00 +0300

    Main function cleaning
    +Generate dictionary function;
    +Menu function;
    +Program loop;
    +Input function
    TODO:
    Sort function;
    Sum function;

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Collections.Generic;
 
 class Program
@@ -7,86 +6,55 @@ class Program
     static void Main()
     {
         Console.Title = "Teltonika IoS";
+        
+        Menu();
 
-        Console.Write("Enter main list lenght: ");
-        var input = Console.ReadLine();//string input;
-        int len;//lenght of main list
+        //Dictionary<int, List<int>> Dict = GenerateDictionary();
 
-        while (!int.TryParse(input, out len))
-        {
-            Console.Clear();
-            Console.Write("Enter main list lenght: ");
-            input = Console.ReadLine();
-        }//amount of main list elements
-
-        var rand = new Random();
-
-        var Dict = new Dictionary<int, List<int>>();
-
-        for (int i = 0; i < len; i++)
-        {
-            int main = rand.Next(999);
-            var SubList = new List<int>();
-            //int sub = 3;
-            int sub = rand.Next(10);//amount of SubList elements
-
-            for (int j = 0; j < sub; j++)
-            {
-                SubList.Add(rand.Next(999));//filling values of SubList
-            }
-            Dict.Add(main, SubList);
-        }
-
-        Display(Dict);
-        Console.Write("Press eny key to continue...");
+        //Display(Dict);
+        Console.Write("\nPress eny key to continue...");
         Console.ReadKey(true); 
+    }
 
-        /*
-        var MainList = new List<List<int>>();
+    private static int Menu()
+    {
+        Dictionary<int, List<int>> Dict = GenerateDictionary();
 
-        Console.Write("Enter main list lenght: ");
-        var input = Console.ReadLine();//string input;
-        int len;//lenght of main list
-        var rand = new Random();
-
-        while (!int.TryParse(input, out len))
+        while(true)
         {
+            Console.WriteLine("1. Generate new list with sublists");
+            Console.WriteLine("2. Display list and sublists");
+            Console.WriteLine("3. Sort chosen list elements");
+            Console.WriteLine("4. Sum chosen list elements");
+            Console.WriteLine("0. Exit");
+                       
+            int choice = Input("Enter choice(int): ");
+
+            //int cou = Dict.Count;                   
+
+            switch (choice)
+            {
+                case 1:
+                    Dict.Clear();
+                    Dict = GenerateDictionary();
+                    break;
+                case 2:
+                    Display(Dict);
+                    break;
+                case 3:
+                    //Dict = SortDictionary(Dict);
+                    break;
+                case 0:
+                    return 0;
+
+                default:
+                    Console.WriteLine("Wrong input!");
+                    break;
+            }
+
+            Console.Write("\nPress eny key to continue...");
+            Console.ReadKey(true);
             Console.Clear();
-            Console.Write("Enter main list lenght: ");
-            input = Console.ReadLine();
-        }//amount of main list elements
-
-        for (int i = 0; i < len; i++)
-        {
-            var SubList = new List<int>();
-            int sub = rand.Next(1, 20);//amount of SubList elements
-            for (int j = 0; j < sub; j++)
-            {
-                SubList.Add(rand.Next(1000));//filling values of SubList
-            }
-            MainList.Add(SubList);
-        }
-        Display(MainList);
-        Console.Write("Press eny key to continue...");
-        Console.ReadKey(true);
-        */
-    }
-
-    static void Menu()
-    {
-        Console.WriteLine("1 ");
-    }
-
-    static void Display(List<List<int>> list)
-    {
-        Console.WriteLine("List: ");
-        foreach(var sublist in list)
-        {
-            foreach(var value in sublist)
-            {
-                Console.Write(value + " ");
-            }
-            Console.WriteLine();
         }
     }
 
@@ -94,25 +62,99 @@ class Program
     {
         Console.WriteLine("List: ");
 
-        var MainListElem = new List<int>(dict.Keys);//keys to list, basically main list elements
+        List<int> MainListElem = new List<int>(dict.Keys);//keys to list, basically main list elements
         
         char MainItem = 'A';
 
-        List<int> sublist;
-
-        foreach(var value in MainListElem)
+        foreach (int value in MainListElem)
         {
             Console.WriteLine("Item " + MainItem + " " + value);
             char SubItem = 'a';
-            dict.TryGetValue(value, out sublist);
+            dict.TryGetValue(value, out List<int> sublist);
 
-            foreach(var val in sublist)
+            foreach (int val in sublist)
             {
-                Console.WriteLine("    Item " + MainItem + SubItem + " " + val);
+                Console.WriteLine(" Subitem " + MainItem + SubItem + " " + val);
                 SubItem++;
             }
             MainItem++;
         }
+    }
+
+    static Dictionary<int, List<int>> GenerateDictionary()
+    {
+        var dict = new Dictionary<int, List<int>>();
+        var rand = new Random();
+        
+        int len = Input("Enter main list lenght(int): ");//lenght of main list
+        
+        for (int i = 0; i < len; i++)
+        {
+            int main = rand.Next(1, 999);//values of MainList
+            var SubList = new List<int>();
+            int sub = rand.Next(10);//amount of SubList elements
+
+            for (int j = 0; j < sub; j++)
+                SubList.Add(rand.Next(1, 999));//values of SubList
+
+            dict.Add(main, SubList);
+        }
+        return dict;
+    }
+
+    static int SumList(List<int> list)
+    {
+        
+        return 0;
+    }
+
+    /*static Dictionary<int, List<int>> SortDictionary(Dictionary<int, List<int>> dict)
+    {
+        var ListToSort = new List<int>();
+
+        var newDict = new Dictionary<int, List<int>>();
+
+        Display(dict);
+
+        var input = Console.ReadLine();
+
+        Console.WriteLine("\nWhatlist You wan to sort?");
+        Console.WriteLine("If you want to sort MainList (Type \"m\")\n" +
+                          "If you want to sort sublist enter main list element");
+
+
+
+        if(input == "m")
+        {
+            int cou = dict.Count;
+            List<int> MainListElem = new List<int>(dict.Keys);
+            MainListElem.Sort();
+            for(int i = 0; i < cou; i++)
+            {
+                newDict.Add(MainListElem[i], );
+            }
+        }
+
+
+        return newDict;
+    }*/
+
+    static int Input(string msg)
+    {
+        Console.Write(msg);
+        var stringinp = Console.ReadLine();
+        int intinp;
+
+        while (!int.TryParse(stringinp, out intinp))
+        {
+            Console.Clear();
+            Console.Write(msg);
+            stringinp = Console.ReadLine();
+        }
+
+        Console.WriteLine();
+
+        return intinp;
     }
 }
 
